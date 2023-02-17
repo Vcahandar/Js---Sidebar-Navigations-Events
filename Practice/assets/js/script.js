78240ee8e29bf1d8970dc2895045f411d35c5468
unknown <jahandarmv@code.edu.az>
2023-02-17 21:02:37 +0400

    new task

---
+++ Practice/assets/js/script.js
@@ -0,0 +1,71 @@
+"use strict"
+
+// let sidebar =document.querySelector(".sidebar");
+
+// let closeIcon=document.querySelector(".sidebar .close-icon");
+
+// let openIcon=document.querySelector(".sidebar .open-icon");
+
+// openIcon.addEventListener("click",function(){
+//     sidebar.classList.remove("hide-sidebar")
+//     closeIcon.classList.remove("d-none")
+//     this.classList.add("d-none")
+
+// })
+
+// closeIcon.addEventListener("click",function(){
+//     sidebar.classList.add("hide-sidebar")
+//     openIcon.classList.remove("d-none")
+//     this.classList.add("d-none")
+// })
+
+
+
+
+let h1=document.querySelector("h1");
+let span=document.querySelector("span")
+
+// h1.parentNode.parentNode.parentNode.style.backgroundColor="red"
+
+
+// console.log(h1.nextElementSibling.firstElementChild.innerText);
+
+// console.log(h1.previousSibling);
+
+// console.log(h1.previousElementSibling);
+
+// let elems= span.closest(".item").children;
+// console.log(elems);
+
+// for (const item of elems) {
+//     item.style.color="blue"
+// }
+
+// let button=document.querySelector("button");
+
+// button.addEventListener("click",function(){
+//     this.parentNode.style.backgroundColor="blue"
+// })
+
+
+// let input=document.querySelector("#blog input");
+
+// input.addEventListener("blur",function(){
+//     this.value=""
+// })
+
+// input.addEventListener("focus",function(){
+//     this.value=""
+// })
+
+// input.addEventListener("change",function(){
+//     console.log("input");
+// })
+
+// input.addEventListener("keydown",function(){
+//     console.log("input");
+// })
+
+// input.addEventListener("keyup",function(){
+//     console.log("input");
+// })
